228b7a0d0eb1a7e72db945076aeed3d8a9b7dc4c
Kneasle <kneasle@gmail.com>
2021-01-21 12:13:57 +0000

    Add correction for HDPI displays

--- www/index.js
+++ www/index.js
@@ -1,8 +1,36 @@
-// Some lovely global variables
+/* ===== CONSTS ===== */
+
+// The 'Device Pixel Ratio'.  For screens with lots of pixels, `1px` might correspond to multiple
+// real life pixels - so dpr provides that scale-up
+const dpr = window.devicePixelRatio || 1;
+
+/* ===== GLOBAL VARIABLES ===== */
+
+// Variables set in the `start()` function
 let canv, ctx;
 
 function onWindowResize() {
-    console.log(window.innerWidth, window.innerHeight);
+    // Set the canvas size according to its new on-screen size
+    var rect = canv.getBoundingClientRect();
+    canv.width = rect.width * dpr;
+    canv.height = rect.height * dpr;
+
+    // Request a frame to be drawn
+    draw();
+}
+
+function draw() {
+    // Clear the screen and correct for HDPI displays
+    ctx.save();
+    ctx.clearRect(0, 0, canv.width, canv.height);
+    ctx.scale(dpr, dpr);
+
+    ctx.beginPath();
+    ctx.arc(100, 100, 80, 0, Math.PI * 2);
+    ctx.stroke();
+
+    // Reset the canvas' transform matrix so that the next frame is rendered correctly
+    ctx.restore();
 }
 
 function start() {
@@ -13,7 +41,7 @@ function start() {
     window.addEventListener('resize', onWindowResize);
     onWindowResize();
 
-    ctx.fillRect(10, 10, 30, 30);
-
     console.log(reverse("Hello"));
+
+    window.requestAnimationFrame(draw);
 }
