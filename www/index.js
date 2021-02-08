1c7ec509b2a0b397d6b882f1c734093848b6f6ae
Kneasle <kneasle@gmail.com>
2021-02-08 14:52:13 +0000

    Add viewport control

--- www/index.js
+++ www/index.js
@@ -21,7 +21,9 @@ let canv, ctx;
 let start_time;
 
 // Viewport controls
-// ...
+let viewport = { x: 0, y: 0, w: 100, h: 100 };
+
+/* ===== DRAWING CODE ===== */
 
 function drawRow(x, y, annot_row) {
     // Set values that are the same across all the bells
@@ -55,41 +57,33 @@ function drawRow(x, y, annot_row) {
     ctx.fillText(annot_row.method_str(), x + bells.length * COL_WIDTH + RIGHT_MARGIN_WIDTH, text_baseline);
 }
 
-function onWindowResize() {
-    // Set the canvas size according to its new on-screen size
-    var rect = canv.getBoundingClientRect();
-    canv.width = rect.width * dpr;
-    canv.height = rect.height * dpr;
-
-    // Request a frame to be drawn
-    window.requestAnimationFrame(frame);
-}
-
 function draw() {
+    // Generate the width and height of the window in user-defined pixels ('px' from CSS)
+    // Move the viewport
+    const elapsed_time = (Date.now() - start_time) / 1000;
+    viewport.y = elapsed_time * 50;
+
     // Clear the screen and correct for HDPI displays
     ctx.save();
     ctx.clearRect(0, 0, canv.width, canv.height);
     ctx.scale(dpr, dpr);
+    // Move so that the camera's origin is in the centre of the screen
+    ctx.translate(viewport.w / 2, viewport.h / 2);
+    ctx.translate(-viewport.x, -viewport.y);
 
     // Calculate viewport for row culling 
-    const viewport = {
-        l: 0, t: 0,
-        r: canv.getBoundingClientRect().width,
-        b: canv.getBoundingClientRect().height,
-    };
-
-    const elapsed_time = (Date.now() - start_time) / 1000;
-
     const frag = Frag.example();
     for (let i = 0; i < frag.len(); i++) {
         const annot_row = frag.get_row(i);
-        const y = 200 - elapsed_time * 50 + ROW_HEIGHT * i;
+        const y = -200 + ROW_HEIGHT * i;
+        const x = -100;
         // Only draw the row if it's actually on the screen
-        if (y < viewport.t - VIEW_CULLING_EXTRA_SIZE || y > viewport.b + VIEW_CULLING_EXTRA_SIZE) {
+        if (y < viewport.y - viewport.h / 2 - VIEW_CULLING_EXTRA_SIZE
+         || y > viewport.y + viewport.h / 2 + VIEW_CULLING_EXTRA_SIZE) {
             continue;
         }
         // Draw the row if it hasn't been culled
-        drawRow(100, y, annot_row);
+        drawRow(x, y, annot_row);
     }
 
     // Reset the canvas' transform matrix so that the next frame is rendered correctly
@@ -100,6 +94,21 @@ function frame() {
     draw();
 
     // window.requestAnimationFrame(frame);
+}
+
+/* ===== CALLBACKS ===== */
+
+function onWindowResize() {
+    // Set the canvas size according to its new on-screen size
+    var rect = canv.getBoundingClientRect();
+    canv.width = rect.width * dpr;
+    canv.height = rect.height * dpr;
+
+    viewport.w = rect.width;
+    viewport.h = rect.height;
+
+    // Request a frame to be drawn
+    window.requestAnimationFrame(frame);
 }
 
 function start() {
