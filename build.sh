ffcb912eaaec776a1ec5fe0297eb6517a557c085
Kneasle <kneasle@gmail.com>
2021-02-09 11:01:19 +0000

    Allow build script to be executed from anywhere in the git repo

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
+#!bash
+
+# Move to the project root
+ROOT=$(git rev-parse --show-toplevel)
+cd $ROOT
+
 # Build the Rust library to WASM
 cd proj
 cargo build --target wasm32-unknown-unknown
 wasm-bindgen --target web --no-typescript --out-dir . ~/.build/rust/wasm32-unknown-unknown/debug/proj.wasm
 wasm-gc proj_bg.wasm
+cd $ROOT
 
 # Copy all files into the out/ directory
-cd ..
 mv proj/proj.js out/
 mv proj/proj_bg.wasm out/
 
+# Make sure that the files in the www/ directory are served along with the compiled wasm
 cd out/
 ln -sf ../www/* .
-cd ..
+cd $ROOT
