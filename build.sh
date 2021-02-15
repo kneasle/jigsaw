21c93bfc5a5864aae5c4efe9666fa5dbf2568852
Kneasle <kneasle@gmail.com>
2021-02-15 13:38:38 +0000

    Improve build script

--- build.sh
+++ build.sh
@@ -4,18 +4,18 @@
 ROOT=$(git rev-parse --show-toplevel)
 cd $ROOT
 
+# Make sure that the files in the www/ directory are served along with the compiled wasm
+mkdir -p out/
+cd out/
+ln -sf ../www/* .
+cd $ROOT
+
 # Build the Rust library to WASM
 cd proj
 cargo build --target wasm32-unknown-unknown
 wasm-bindgen --target web --no-typescript --out-dir . ~/.build/rust/wasm32-unknown-unknown/debug/proj.wasm
 wasm-gc proj_bg.wasm
 cd $ROOT
-
 # Copy all files into the out/ directory
 mv proj/proj.js out/
 mv proj/proj_bg.wasm out/
-
-# Make sure that the files in the www/ directory are served along with the compiled wasm
-cd out/
-ln -sf ../www/* .
-cd $ROOT
